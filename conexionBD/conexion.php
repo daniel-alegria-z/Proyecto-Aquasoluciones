acc1a1925947c266d9da7b3f49bcbfd37603e29e
Daniel Alegría <137464084+daniel-alegria-z@users.noreply.github.com>
2025-05-06 21:36:24 -0500

    Update conexion.php

--- conexionBD/conexion.php
+++ conexionBD/conexion.php
@@ -1,21 +1,20 @@
 <?php
 class ConexionBD {
     function conexionBD() {
-        $host     = getenv("postgres.railway.internal");
-        $port     = getenv("5432");
-        $bdname   = getenv("railway");
-        $username = getenv("postgres");
-        $pasword  = getenv("CCCqUGwkjnhnYsnUsYVHApiiYwYfdeSf");
+        $host     = getenv("PGHOST");
+        $port     = getenv("PGPORT");
+        $bdname   = getenv("PGDATABASE");
+        $username = getenv("PGUSER");
+        $pasword  = getenv("PGPASSWORD");
 
         try {
             $conn = new PDO("pgsql:host=$host;port=$port;dbname=$bdname", $username, $pasword);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
+            return $conn;
         } catch(PDOException $e) {
             echo "No se pudo conectar a la base de datos: " . $e->getMessage();
-
+            return null;
         }
-        return $conn;
     }
 }
 ?>
