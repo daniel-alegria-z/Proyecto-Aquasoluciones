e6cd1857a1c36cb449ef5b29e96dbe7915be1a20
Daniel Alegría <137464084+daniel-alegria-z@users.noreply.github.com>
2025-05-06 21:30:20 -0500

    Update conexion.php

--- conexionBD/conexion.php
+++ conexionBD/conexion.php
@@ -10,11 +10,12 @@ class ConexionBD {
         try {
             $conn = new PDO("pgsql:host=$host;port=$port;dbname=$bdname", $username, $pasword);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $conn;
+            
         } catch(PDOException $e) {
             echo "No se pudo conectar a la base de datos: " . $e->getMessage();
-            return null;
+
         }
+        return $conn;
     }
 }
 ?>
