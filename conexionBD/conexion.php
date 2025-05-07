3bebd46882d1fe129fb8e4e3d53b8b7042f777eb
Daniel Alegría <137464084+daniel-alegria-z@users.noreply.github.com>
2025-05-06 21:26:54 -0500

    Update conexion.php

--- conexionBD/conexion.php
+++ conexionBD/conexion.php
@@ -1,19 +1,20 @@
 <?php
-    class ConexionBD {
-        function conexionBD() {
-            $host = "host.docker.internal";
-            $bdname = "aquasoluciones";
-            $username = "postgres";
-            $pasword = "1234";
+class ConexionBD {
+    function conexionBD() {
+        $host     = getenv("postgres.railway.internal");
+        $port     = getenv("5432");
+        $bdname   = getenv("railway");
+        $username = getenv("postgres");
+        $pasword  = getenv("CCCqUGwkjnhnYsnUsYVHApiiYwYfdeSf");
 
-            try {
-                $conn = new PDO("pgsql:host=$host;dbname=$bdname", $username, $pasword);
-            } catch(PDOException $e) {
-                echo ("No se pudo conectar a la base de datos" . $e);
-            }
-       
-        return $conn;
+        try {
+            $conn = new PDO("pgsql:host=$host;port=$port;dbname=$bdname", $username, $pasword);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch(PDOException $e) {
+            echo "No se pudo conectar a la base de datos: " . $e->getMessage();
+            return null;
         }
     }
-
+}
 ?>
