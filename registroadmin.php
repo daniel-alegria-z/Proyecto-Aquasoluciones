becfa42b5f951a6a92869c5a22930f282a1db940
Daniel Alegría <137464084+daniel-alegria-z@users.noreply.github.com>
2025-05-06 22:04:24 -0500

    Update registroadmin.php

--- registroadmin.php
+++ registroadmin.php
@@ -1,6 +1,7 @@
 
 <?php
-include 'includes/formulario.php';
-include 'includes/logic.php';
-include 'includes/footer.php';
+echo 'PGHOST: ' . getenv('PGHOST') . '<br>';
+echo 'PGDATABASE: ' . getenv('PGDATABASE') . '<br>';
+echo 'PGUSER: ' . getenv('PGUSER') . '<br>';
+echo 'PGPASSWORD: ' . getenv('PGPASSWORD') . '<br>';
 ?>
