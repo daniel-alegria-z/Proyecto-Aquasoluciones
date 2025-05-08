d52b312b4755668e890c26675d13a705d9c994f4
alegria666 <danialegria00@gmail.com>
2025-05-07 19:12:33 -0500

    correcciones menores

--- registroadmin.php
+++ registroadmin.php
@@ -1,9 +1,7 @@
 
 <?php
-echo '<div class="Cargarpag">';
 echo '<div class="cargar"><img src="assets/imagenes/loading.gif" alt="#"/></div>';
 include 'includes/formulario.php';
 include 'includes/logic.php';
 include 'includes/footer.php';
-echo '</div>';
 ?>
